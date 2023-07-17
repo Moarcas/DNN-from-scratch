898779eedc00b92896aaa6321e3cd5623cb5d715
cosmin <cosminmoarcas01@gmail.com>
2023-07-17 12:18:09 +0300

    read data

---
+++ dnn_app_utils_v3.py
@@ -0,0 +1,285 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+
+
+def sigmoid(Z):
+    """
+    Implements the sigmoid activation in numpy
+    
+    Arguments:
+    Z -- numpy array of any shape
+    
+    Returns:
+    A -- output of sigmoid(z), same shape as Z
+    cache -- returns Z as well, useful during backpropagation
+    """
+    
+    A = 1/(1+np.exp(-Z))
+    cache = Z
+    
+    return A, cache
+
+def relu(Z):
+    """
+    Implement the RELU function.
+
+    Arguments:
+    Z -- Output of the linear layer, of any shape
+
+    Returns:
+    A -- Post-activation parameter, of the same shape as Z
+    cache -- a python dictionary containing "A" ; stored for computing the backward pass efficiently
+    """
+    
+    A = np.maximum(0,Z)
+    
+    assert(A.shape == Z.shape)
+    
+    cache = Z 
+    return A, cache
+
+
+def relu_backward(dA, cache):
+    """
+    Implement the backward propagation for a single RELU unit.
+
+    Arguments:
+    dA -- post-activation gradient, of any shape
+    cache -- 'Z' where we store for computing backward propagation efficiently
+
+    Returns:
+    dZ -- Gradient of the cost with respect to Z
+    """
+    
+    Z = cache
+    dZ = np.array(dA, copy=True) # just converting dz to a correct object.
+    
+    # When z <= 0, you should set dz to 0 as well. 
+    dZ[Z <= 0] = 0
+    
+    assert (dZ.shape == Z.shape)
+    
+    return dZ
+
+def sigmoid_backward(dA, cache):
+    """
+    Implement the backward propagation for a single SIGMOID unit.
+
+    Arguments:
+    dA -- post-activation gradient, of any shape
+    cache -- 'Z' where we store for computing backward propagation efficiently
+
+    Returns:
+    dZ -- Gradient of the cost with respect to Z
+    """
+    
+    Z = cache
+    
+    s = 1/(1+np.exp(-Z))
+    dZ = dA * s * (1-s)
+    
+    assert (dZ.shape == Z.shape)
+    
+    return dZ
+
+
+def load_data():
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+
+def initialize_parameters(n_x, n_h, n_y):
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+
+    assert(W1.shape == (n_h, n_x))
+    assert(b1.shape == (n_h, 1))
+    assert(W2.shape == (n_y, n_h))
+    assert(b2.shape == (n_y, 1))
+
+    parameters = {
+        "W1": W1,
+        "W2": W2,
+        "b1": b1,
+        "b2": b2
+    }
+
+    return parameters
+
+    
+def initialize_paramaters_deep(layer_dims):
+    np.random.seed(3)
+
+    parameters = {}
+    for L in range(1, len(layer_dims)):
+        parameters["W" + str(L)] = np.random.randn(layer_dims[L], layer_dims[L - 1]) * 0.01
+        parameters["b" + str(L)] = np.zeros((layer_dims[L], 1))
+
+        assert(parameters["W" + str(L)] == (layer_dims[L], layer_dims[L - 1]))
+        assert(parameters["b" + str(L)] == (layer_dims[L], 1))
+
+    return parameters
+
+def linear_forward(A, W, b):
+    Z = np.dot(W, A) + b
+    cache = (A, W, b)
+
+    assert(Z.shape == (W.shape[0], A.shape[1]))
+
+    return Z, cache
+
+def linear_activation_forward(A_prev, W, b, activation):
+    Z, linear_cache = linear_forward(A_prev, W, b)
+
+    if activation == "sigmoid":
+        A, activation_cache = sigmoid(Z)
+    elif activation == "relu":
+        A, activation_cache = relu(Z)
+
+    assert(A.shape == (W.shape[0], A_prev.shape[1]))
+    cache = (linear_cache, activation_cache)
+
+    return A, cache
+
+def L_model_forward(X, parameters):
+    L = len(parameters) // 2 
+
+    caches = []
+    AL = X
+
+    for l in range(1, L):
+        WL = parameters["W" + str(l)]
+        bL = parameters["b" + str(l)]
+        AL, cache = linear_activation_forward(AL, WL, bL, "relu")
+        caches.append(cache)
+
+    WL = parameters["W" + str(L)]
+    bL = parameters["b" + str(L)]
+    AL, cache = linear_activation_forward(AL, WL, bL, "sigmoid")
+    caches.append(cache)
+
+    return AL, caches
+
+def compute_cost(AL, Y):
+    m = Y.shape[1]
+    cost = (np.dot(Y, np.log(AL).T) + np.dot((1 - Y), np.log(1 - AL).T)) * (-1/m)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    return cost
+
+def linear_backward(dZ, cache):
+    A_prev, W, b = cache
+    m = dZ.shape[1]
+    
+    dA_prev = np.dot(W.T, dZ)
+    dW = np.dot(dZ, A_prev.T) * (1 / m)
+    db = np.sum(dZ, axis=1, keepdims=True) * (1 / m)
+
+    assert(dA_prev.shape == A_prev.shape)
+    assert(dW.shape == W.shape)
+    assert(db.shape == b.shape)
+    return dA_prev, dW, db
+
+def linear_activation_backward(dA, cache, activation):
+    linear_cache, activation_cache = cache
+
+    if activation == "sigmoid": 
+        dZ = sigmoid_backward(dA, activation_cache)
+    elif activation == "relu":
+        dZ = relu_backward(dA, activation_cache)
+
+    dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    return dA_prev, dW, db
+
+def L_model_backward(AL, Y, caches):
+    grads = {}
+    L = AL.shape[1]
+    Y = Y.reshape(AL.shape)
+    dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+
+    grads["dA" + str(L - 1)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL, caches[L - 1], "sigmoid")
+
+    for l in range(L - 1, 0, -1):
+        dA = grads["dA" + str(l)]
+        grads["dA" + str(l - 1)], grads["dW" + str(l)], grads["db" + str(l)] = linear_activation_backward(dA, caches[l - 1], "relu")
+
+    return grads
+
+def update_parameters(parameters, grads, learning_rate):
+    L = len(parameters) // 2
+
+    for l in range(1, L + 1):
+        dW, db = grads["dW" + str(l)], grads["db" + str(l)]
+        parameters["W" + str(l)] -= learning_rate * dW
+        parameters["b" + str(l)] -= learning_rate * db
+
+    return parameters
+
+def predict(X, y, parameters):
+    """
+    This function is used to predict the results of a  L-layer neural network.
+    
+    Arguments:
+    X -- data set of examples you would like to label
+    parameters -- parameters of the trained model
+    
+    Returns:
+    p -- predictions for the given dataset X
+    """
+    
+    m = X.shape[1]
+    n = len(parameters) // 2 # number of layers in the neural network
+    p = np.zeros((1,m))
+    
+    # Forward propagation
+    probas, caches = L_model_forward(X, parameters)
+
+    
+    # convert probas to 0/1 predictions
+    for i in range(0, probas.shape[1]):
+        if probas[0,i] > 0.5:
+            p[0,i] = 1
+        else:
+            p[0,i] = 0
+    
+    #print results
+    #print ("predictions: " + str(p))
+    #print ("true labels: " + str(y))
+    print("Accuracy: "  + str(np.sum((p == y)/m)))
+        
+    return p
+
+def print_mislabeled_images(classes, X, y, p):
+    """
+    Plots images where predictions and truth were different.
+    X -- dataset
+    y -- true labels
+    p -- predictions
+    """
+    a = p + y
+    mislabeled_indices = np.asarray(np.where(a == 1))
+    plt.rcParams['figure.figsize'] = (40.0, 40.0) # set default size of plots
+    num_images = len(mislabeled_indices[0])
+    for i in range(num_images):
+        index = mislabeled_indices[1][i]
+        
+        plt.subplot(2, num_images, i + 1)
+        plt.imshow(X[:,index].reshape(64,64,3), interpolation='nearest')
+        plt.axis('off')
+        plt.title("Prediction: " + classes[int(p[0,index])].decode("utf-8") + " \n Class: " + classes[y[0,index]].decode("utf-8"))
